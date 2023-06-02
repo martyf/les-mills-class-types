1083e64bf2c44e91aa8c728df183d24ac1d629af
Marty Friedel <1491079+martyf@users.noreply.github.com>
2023-06-02 11:25:12 +1000

    Remove fonts

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,10 +15,6 @@ class ServiceProvider extends AddonServiceProvider
         __DIR__.'/../dist/css/les-mills-class-types.css'
     ];
 
-    protected $publishables = [
-        __DIR__.'/../dist/css/fonts' => 'css/fonts'
-    ];
-
     public function bootAddon()
     {
         Augmentor::addExtension('lesMillsClassType', new LesMillsClassType());
