134e5770832d409c3ba931d2ae1b1126eb2db51d
Martin Dub <martin.dub@dek-cz.com>
2023-07-11 13:36:38 +0200

    New icon

--- resources/js/bard.js
+++ resources/js/bard.js
@@ -9,7 +9,7 @@ Statamic.$bard.buttons((buttons, button) => {
         args: {
             key: ""
         },
-        html: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="currentColor"><path d="M7.6 17.295c-.03.32.22.76.65.76H22.5l-.76 4.82H3.87c-1.76 0-2.47-1.6-2.36-2.58l2.93-19.17h5.58l-2.41 16.17"/></svg>',
+        html: '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 576 512" fill="currentColor"><path d="M339.3 367.1c27.3-3.9 51.9-19.4 67.2-42.9L568.2 74.1c12.6-19.5 9.4-45.3-7.6-61.2S517.7-4.4 499.1 9.6L262.4 187.2c-24 18-38.2 46.1-38.4 76.1L339.3 367.1zm-19.6 25.4l-116-104.4C143.9 290.3 96 339.6 96 400c0 3.9 .2 7.8 .6 11.6C98.4 429.1 86.4 448 68.8 448H64c-17.7 0-32 14.3-32 32s14.3 32 32 32H208c61.9 0 112-50.1 112-112c0-2.5-.1-5-.2-7.5z"/></svg>',
         component: LesMillsClassTypesMenu
     });
 });
